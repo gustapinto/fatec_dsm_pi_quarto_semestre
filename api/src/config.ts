150be5184d4e7e2eb08b5fbc1838b31f8b1f82e1
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-10-26 20:20:37 -0300

    Adiciona classe para gerenciar configs

---
+++ api/src/config.ts
@@ -0,0 +1,28 @@
+/**
+ * Classe responsável por gerenciar as configurações do sistema
+ */
+export class Config {
+    /**
+     * Obtém a app id do Openweather a partir das variáveis de ambiente
+    */
+    static getOpenWheaterAppId(): string {
+        const appId = process.env.OPENWEATHER_API_KEY
+
+        return appId == undefined ? '' : appId
+    }
+
+    /**
+     * Obtém o objeto com os parâmetros de conexão com o banco de dados
+    */
+    static getDatabaseConfig(): DatabaseConfig {
+        const config = {
+            user: process.env.POSTGRES_USER!,
+            host: process.env.POSTGRES_HOST!,
+            database: process.env.POSTGRES_DB!,
+            password: process.env.POSTGRES_PASSWORD!,
+            port: parseInt(process.env.POSTGRES_PORT!)
+        }
+
+        return config
+    }
+}
---
+++ api/src/models/DatabaseConfig.ts
@@ -0,0 +1,10 @@
+/**
+ * Estrutura de configuração do banco de dados
+*/
+interface DatabaseConfig {
+    user: string
+    host: string
+    database: string
+    password: string
+    port: number
+}
