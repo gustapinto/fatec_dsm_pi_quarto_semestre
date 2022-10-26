5e97c03e36664e69cc766035fd3cceb7bb9ee6fc
Karen Letícia Alexandre <heroace44@gmail.com>
2022-10-26 19:39:27 -0300

    Adicionados arquivos do aplicativo.

---
+++ mobile/app/src/main/java/com/example/project_pi/dispositivos.kt
@@ -0,0 +1,11 @@
+package com.example.project_pi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dispositivos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dispositivos)
+    }
+}
---
+++ mobile/app/src/main/java/com/example/project_pi/inicio.kt
@@ -0,0 +1,11 @@
+package com.example.project_pi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
