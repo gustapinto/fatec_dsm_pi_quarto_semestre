2da38cfbae407833c3dc24a8148b68e947eaad25
Karen Letícia Alexandre <heroace44@gmail.com>
2022-10-27 20:35:40 -0300

    Continuação da criação dos layouts das telas do mobile

--- mobile/app/src/main/java/com/example/project_pi/inicio.kt
+++ mobile/app/src/main/java/com/example/project_pi/inicio.kt
@@ -1,11 +1,20 @@
 package com.example.project_pi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_inicio.*
 
 class inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+
+        buttonStart.setOnClickListener {
+            val intent = Intent(applicationContext, dispositivos::class.java)
+
+            startActivity(intent)
+        }
     }
 }
