0b29d65000bce35b48b2b65686cd37ddf35b85f2
Karen Letícia Alexandre <heroace44@gmail.com>
2022-11-02 16:10:24 -0300

    Adicionados todas as activitys necessarias para o mobile

---
+++ mobile/app/src/main/java/com/example/project_pi/addDispositivos.kt
@@ -0,0 +1,11 @@
+package com.example.project_pi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addDispositivos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_dispositivos)
+    }
+}
