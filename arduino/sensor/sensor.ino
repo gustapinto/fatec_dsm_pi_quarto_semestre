7edc7d44ae61fb4401512f43ef705abe0253d189
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-11-06 15:21:59 -0300

    Add sensor final code (#24)
    * Corrige função de login
    
    * Adiciona código para criar registros pelo nodemcu

--- arduino/sensor/sensor.ino
+++ arduino/sensor/sensor.ino
@@ -1,11 +1,11 @@
 // Bibiliotecas do sensor de temperatura e umidade
+#include <ArduinoJson.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 // Bibiliotecas para conexão com o wifi e requisições HTTP
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
 
 #define DHT_PIN 4
 #define DELAY_MS 5000
@@ -14,14 +14,11 @@
 #define WIFI_PASSWORD "zigur@t3"
 
 // Define o código da placa e a url base da api
-String const CODE = "12345678";
-String const API_BASE_URL = "https://termostato.programame.dev";
+String const CODE = "123456";
+String const API_BASE_URL = "https://termostato.programame.dev/api";
 
 // Configura o sensor DHT
 DHT dht(DHT_PIN, DHT11);
-// Configura o cliente HTTP para realizar as requisições
-WiFiClient client;
-HTTPClient http;
 
 /// Obtém os dados de temperatura e umidade do sensor
 std::tuple<float, float> getSensorData()
@@ -33,47 +30,56 @@ std::tuple<float, float> getSensorData()
 }
 
 /// Faz login na API e retorna o Token JWT correspondente
-String login() {
-    StaticJsonDocument<400> response;
+String login()
+{
+    WiFiClientSecure client;
+    HTTPClient http;
+    StaticJsonDocument<384> response;
 
-    http.begin(client, API_BASE_URL + "/api/login");
+    String endpoint = API_BASE_URL + "/auth";
+    String payload = "{\"arduinoCode\": " + CODE + "}";
+
+    client.setInsecure();  // Desabilita validação de certificado
+    // <std::string>.c_str() converte uma std::string para char*
+    http.begin(client, endpoint.c_str());
     http.addHeader("content-type", "application/json");
 
-    String payload = "{\"arduinoCode\": " + CODE + "}";
-    int responseCode = http.POST(payload);
-
-    if (responseCode != HTTP_CODE_OK) {
+    int httpCode = http.POST(payload.c_str());
+    if (httpCode >= 400) {
         return "";
     }
 
-    String responseBody = http.getString();
+    String body = http.getString();
     http.end();
 
-    DeserializationError error = deserializeJson(response, responseBody);
+    DeserializationError error = deserializeJson(response, body);
     if (error) {
         return "";
     }
 
-    String tokenType = response["result"]["type"];
-    String token = response["result"]["token"];
-
-    return tokenType + " " + token;
+    return response["result"];
 }
 
-/// Faz a requisição para realizar a criação de um novo registro
-/// de temperatura
-void createRecord(float temperature, float humidity, String token) {
-    http.begin(client, API_BASE_URL + "/api/arduino");
+/// Cria um novo registro
+void createRecord(String token, String temperature, String humidity)
+{
+    WiFiClientSecure client;
+    HTTPClient http;
+
+    String endpoint = API_BASE_URL + "/record";
+    String payload = "{\"arduinoCode\":" + CODE + ",\"temperature\":" + temperature + ",\"humidity\":" + humidity + "}";
+
+    client.setInsecure();
+    http.begin(client, endpoint.c_str());
     http.addHeader("content-type", "application/json");
-    http.addHeader("Authorization", token);
+    http.addHeader("Authorization", token.c_str());
 
-    String payload = "{}";
-    int responseCode = http.POST(payload);
-    if (responseCode != HTTP_CODE_OK) {
-        return;
-    }
+    http.POST(payload.c_str());
 
+    String body = http.getString();
     http.end();
+
+    Serial.println(body);
 }
 
 void setup()
@@ -87,20 +93,27 @@ void setup()
     // Inicializa o wi-fi
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
-        delay(100);
+        Serial.print(".");
+        delay(1000);
     }
+
+    Serial.println();
+    Serial.println("Wi-Fi Conectado");
 }
 
 void loop()
 {
-    auto [temperature, humidity] = getSensorData();
-
     String token = login();
     if (token == "") {
         return;
     }
 
-    createRecord(temperature, humidity, token);
+    auto [temperature, humidity] = getSensorData();
+
+    String temperatureStr = String(temperature, 2);
+    String humidityStr = String(humidity, 2);
+
+    createRecord(token, temperatureStr, humidityStr);
 
     delay(DELAY_MS);
 }
