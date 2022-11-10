2f447f26fbbb32f24b76d135bb4f7922c2438988
Karen Letícia Alexandre <heroace44@gmail.com>
2022-11-10 17:04:11 -0300

    Finalizado o layout da tela de adicionar dispositivos, homde de temperatura e umidade

--- mobile/app/src/main/java/com/example/project_pi/addDispositivos.kt
+++ mobile/app/src/main/java/com/example/project_pi/addDispositivos.kt
@@ -1,11 +1,21 @@
 package com.example.project_pi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_dispositivos.*
+import kotlinx.android.synthetic.main.activity_inicio.*
 
 class addDispositivos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_dispositivos)
+
+
+        buttonContinue.setOnClickListener {
+            val intent = Intent(applicationContext, homeTemp::class.java)
+
+            startActivity(intent)
+        }
     }
 }
