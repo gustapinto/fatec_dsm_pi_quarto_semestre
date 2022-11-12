ee89d70a759f8c2194e98c0c03184fdc3b6ee03d
Karen Letícia Alexandre <heroace44@gmail.com>
2022-11-11 22:10:55 -0300

    Modificações no layout das telas

--- mobile/app/src/main/java/com/example/project_pi/homeUmidade.kt
+++ mobile/app/src/main/java/com/example/project_pi/homeUmidade.kt
@@ -1,11 +1,26 @@
 package com.example.project_pi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home_temp.*
+import kotlinx.android.synthetic.main.activity_home_umidade.*
 
 class homeUmidade : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_umidade)
+
+        homeTempViewUmi.setOnClickListener {
+            val intent = Intent(applicationContext, homeTemp::class.java)
+
+            startActivity(intent)
+        }
+
+        homeConfigViewUmi.setOnClickListener {
+            val intent = Intent(applicationContext, Config::class.java)
+
+            startActivity(intent)
+        }
     }
 }
