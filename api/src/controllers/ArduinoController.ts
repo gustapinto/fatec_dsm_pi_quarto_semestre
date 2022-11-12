a380d795b2f8c25052d2ccd87ae7535c99209922
Gustavo Henrique Pinto <ghp2201@gmail.com>
2022-11-11 21:20:04 -0300

    Merge

--- api/src/controllers/ArduinoController.ts
+++ api/src/controllers/ArduinoController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express"
 import { ArduinoDoesNotExistsException } from "../exceptions/ArduinoDoesNotExistsException"
 import AuthMiddleware from "../middlewares/AuthMiddleware"
 import { ArduinoRepository } from "../repositories/ArduinoRepository"
+import { RecordRepository } from "../repositories/RecordRepository"
 
 /**
  * Controller responsável por criar e apagar registros das placas arduino
@@ -11,9 +12,11 @@ import { ArduinoRepository } from "../repositories/ArduinoRepository"
 @Controller('api/arduino')
 export class ArduinoController {
     private repository: ArduinoRepository
+    private recordRepository: RecordRepository
 
-    constructor(repository: ArduinoRepository) {
+    constructor(repository: ArduinoRepository, recordRepository: RecordRepository) {
         this.repository = repository
+        this.recordRepository = recordRepository
     }
 
     /**
@@ -95,15 +98,34 @@ export class ArduinoController {
     }
 
     /**
-     * Remove a placa arduino do banco de dados
+     * Remove a placa arduino do banco de dados junto com todos os registros de
+     * temperatura associados
     */
     @Delete(':code')
     @Middleware([AuthMiddleware])
     async deleteArduino(req: Request, res: Response): Promise<Response<any>|void> {
         const arduinoCode = req.params.code as string
 
-        /**
-         * TODO
-        */
+        try {
+            await this.recordRepository.deleteRecordWithArduinoCode(arduinoCode)
+            await this.repository.deleteArduino(arduinoCode)
+
+            return res.status(200).json({
+                result: null,
+                message: 'Success deleting the arduino and records'
+            })
+        } catch(error: any) {
+            if (error instanceof ArduinoDoesNotExistsException) {
+                return res.status(400).json({
+                    result: null,
+                    message: `The arduino ${arduinoCode} does not exist, please try again with another arduino code`
+                })
+            }
+
+            return res.status(500).json({
+                result: null,
+                message: error.message,
+            })
+        }
     }
 }
