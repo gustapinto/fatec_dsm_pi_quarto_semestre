4522ff2cbd523779da6c0803cfd7234bf72c5faa
Karen Letícia Alexandre <heroace44@gmail.com>
2022-11-14 17:12:32 -0300

    Adicionado layout das telas de configuração e edição de dispositivo

--- mobile/app/src/main/java/com/example/project_pi/Config.kt
+++ mobile/app/src/main/java/com/example/project_pi/Config.kt
@@ -1,11 +1,20 @@
 package com.example.project_pi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_dispositivos.*
+import kotlinx.android.synthetic.main.activity_config.*
 
 class Config : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
+
+        alterinfo.setOnClickListener {
+            val intent = Intent(applicationContext, alterarInformacoes::class.java)
+
+            startActivity(intent)
+        }
     }
 }
