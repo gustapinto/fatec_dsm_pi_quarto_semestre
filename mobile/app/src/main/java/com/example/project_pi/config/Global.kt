31d61a875979b462dc8fc3d0b9523717d39dbc1b
Gustavo Pinto <ghp2201@gmail.com>
2022-11-23 14:43:34 -0300

    Corrige classe global

--- mobile/app/src/main/java/com/example/project_pi/config/Global.kt
+++ mobile/app/src/main/java/com/example/project_pi/config/Global.kt
@@ -3,5 +3,19 @@ package com.example.project_pi.config
 import android.app.Application
 
 class Global : Application() {
-    var jwtToken: String = ""
+    companion object {
+        private var jwtToken: String = ""
+
+        fun setToken(token: String) {
+            this.jwtToken = token.trim()
+        }
+
+        fun getToken(): String {
+            return this.jwtToken
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
