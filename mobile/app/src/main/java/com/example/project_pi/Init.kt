75ae7ecb8d3a1361cab47d79583eb87dab34d344
Gustavo Pinto <ghp2201@gmail.com>
2022-11-23 14:44:07 -0300

    Aplica classes de callback no Init.kt

--- mobile/app/src/main/java/com/example/project_pi/Init.kt
+++ mobile/app/src/main/java/com/example/project_pi/Init.kt
@@ -1,10 +1,13 @@
 package com.example.project_pi
 
 import android.content.Intent
+import android.media.browse.MediaBrowser.ConnectionCallback
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.project_pi.config.Config
 import com.example.project_pi.dataclasses.GenericResponse
+import com.example.project_pi.services.callbacks.GetConnectionsCallback
+import com.example.project_pi.services.callbacks.HasConnectionsCallback
 import kotlinx.android.synthetic.main.activity_init.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,57 +20,10 @@ class Init : AppCompatActivity() {
         setContentView(R.layout.activity_init)
 
         buttonStart.setOnClickListener {
-            val ctx = this
-            val caller = RetrofitInitializer()
+            RetrofitInitializer()
                 .connectionService()
-                .hasConnections(Config.getAndroidId(ctx))
-
-            caller.enqueue(object : Callback<GenericResponse> {
-                override fun onResponse(call: Call<GenericResponse>, response: Response<GenericResponse>) {
-                    ctx.handleCheckConnectionResponse(response.body()!!)
-                }
-
-                override fun onFailure(call: Call<GenericResponse>, t: Throwable) {
-                    TODO("Not yet implemented")
-                }
-            })
+                .hasConnections(Config.getAndroidId(this))
+                .enqueue(HasConnectionsCallback(this))
         }
     }
-
-    fun handleCheckConnectionResponse(response: GenericResponse) {
-        val deviceAlreadyHasConnections = response.result!! as Boolean
-
-        if (deviceAlreadyHasConnections) {
-            /**
-             * TODO -> Obter a lista de arduinos usando um caller, pega o primeiro arduino e usar ele
-             *         para fazer login
-             * */
-
-            val ctx = this
-            val caller = RetrofitInitializer()
-                .()
-                .hasConnections(Config.getAndroidId(ctx))
-
-            caller.enqueue(object : Callback<GenericResponse> {
-                override fun onResponse(call: Call<GenericResponse>, response: Response<GenericResponse>) {
-                    ctx.handleRequestOfListPlates(response.body()!!)
-                }
-
-                override fun onFailure(call: Call<GenericResponse>, t: Throwable) {
-                    TODO("Not yet implemented")
-                }
-            })
-
-            startActivity(Intent(this, HomeThermometer::class.java))
-        } else {
-            startActivity(Intent(this, NewDevice::class.java))
-        }
-    }
-
-    fun handleRequestOfListPlates(response: GenericResponse){
-        val listPlates = response.result!! as Array<Objects>
-
-        println(listPlates)
-    }
-
 }
